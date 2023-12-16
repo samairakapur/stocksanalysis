d44ecdddee5a5d6340e998e48efddc8351577e9c
samairakapur <148620705+samairakapur@users.noreply.github.com>
2023-12-16 17:56:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ with tab1:
 	col5.image(img4)
 	col6.write("Today, there are roughly 80 major stock exchanges worth a combined $110.2 trillion in value. The world’s top two exchanges, the New York Stock Exchange (NYSE) and the Nasdaq, command 42.4% of global market capitalisation.")
 	col6.write("The visualisation on the left shows the largest stock exchanges in the world, with data from the World Federation of Exchanges (WFE).")
-	col6.write(" The top seven stocks in the world are known as the 'Magnificent Seven' which are megacap companies focused and capitalizing on tech growth trends including AI, cloud computing, and cutting-edge hardware and software.")
+	col6.write(" The top seven stocks in the world are known as the 'Magnificent Seven' (shown in the iage below) which are megacap companies focused and capitalizing on tech growth trends including AI, cloud computing, and cutting-edge hardware and software.")
 	col6.write("The table below shows the companies ranked by their market capitalization on November 6, 2023, alongside their 5-year stock performance")
 	col6.image(img5)
 
@@ -88,7 +88,27 @@ with tab1:
 	col6.write("This directly leads me to my next point regarding the importance of investing") 
 
 	st.subheader("Why should you start investing in stocks?")
-	
+	col7, col8 = st.columns(2, gap = "small") 
+	col7.write(" 3 Grand Benefits of Investing in Stocks Include:")
+	col7.write("1. Ability to achieve financial goals") 
+	col7.write("Investing early allows individual's to harness the power of compounding Compounding refers to the snowball effect where your investment returns generate additional returns over time. The earlier you start, the more time your money has to compound, potentially leading to significant long-term gains. Therefore, investing early is a key trategy for achieving financial goals such as buying a home, funding education, or enjoying a comfortable retirement. The growth potential of investments can assist in reaching these milestones more effectively than relying solely on savings.") 
+	col7.write("2. Ability to protect family from inflation")
+	col7.write("Stock market's returns often significantly outpace the rate of inflation. For example, the long-term inflation rate has run about 3.1% annually since 1913. That compares to a double-digit annual return from stocks. Stocks have been a good way to hedge against inflation.")
+	col7.write("3. Earn a passive income") 
+	col7.write("Many companies pay dividends, or a portion of their profits, to investors. The majority make quarterly dividend payments, although some companies pay monthly dividends. Dividend income can help supplement an investor's paycheck or retirement income.")
+
+	col8.write(" Risks of investing in stock that you should keep at the back of your mind") 
+	col8.write("1. Lack of guarenteed returns") 
+	col8.write("Unlike savings accounts or certain fixed-income investments, the returns on many investments are not guaranteed. Market conditions and the performance of individual assets can vary, leading to uncertainty in returns.")
+	col8.write("2. Market Volatility") 
+	col8.write("Stock prices can be highly volatile, experiencing frequent and sometimes unpredictable fluctuations. This volatility is influenced by various factors such as economic conditions, geopolitical events, and investor sentiment. Rapid and unpredictable changes in stock prices can lead to short-term losses and make it challenging for investors to accurately time their trades.")
+	col8.write("3. Research involvement") 
+	col8.write("Successful investing often requires a good understanding of financial markets, economic factors, and individual securities. Keeping up with research and staying informed about market trends can be time-consuming and challenging for some investors.")
+
+	st.subheader("Useful resources to gain better understanding on stocks + what I learnt') 
+
+	st.subheader("About Me")
+	col9, col10 = st.columns(2, gap = "small") 
 
 with tab2:
 	cmp = st.selectbox('Select the company',('Apple','Starbucks','Microsoft','Cisco','Qualcomm','Meta','Amazon','Tesla','Netflix','Advanced Micro Devices'),key='st1')

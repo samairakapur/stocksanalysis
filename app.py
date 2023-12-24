b16b6f8da219fa756e320c83c82065615b147192
samairakapur <148620705+samairakapur@users.noreply.github.com>
2023-12-24 17:18:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -185,6 +185,10 @@ with tab2:
 	sns.heatmap(corr_df, annot=True, mask=np.tril(corr_df)==0,ax=ax)
 	st.pyplot(fig)
 
+	container7 = st.container(border=True)
+	container7.write("A corerlation matrix can be seen above which shows the relationship between a few stocks. It can be observed that each correlation is positive and between 0 and 1, which indicates that if the price of one stock increases, then the price of the other stock also increases, vise versa. Therefore, a correlation matrix makes it easier for an investor to judge which stock(s) to invest or sell in order to make the most profit.") 
+	container7.write("For example, if the stock market price of Apple goes down, the stock market price of Microsoft will also go down because they have a 0.98 correlation. However, if the stock market predicts that within the next week the market price of an apple stock will skyrocket, the same will also occur for Microsoft. Hence, with the knowledge of the correlation between these 2 stocks, the individaul can make more than double the profits")
+	
 with tab3:
 	st.header("DMA Analysis")
 	col1, col2 = st.columns(2)
